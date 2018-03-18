ca5900a0c31090a519043d3d97795f68313b0bb5
ErikBoesen <me@erikboesen.com>
2018-03-17 23:27:40 -0400

    Remove unnecessary decimal places from training data

--- code/nn/data_generator.cpp
+++ code/nn/data_generator.cpp
@@ -5,12 +5,12 @@
 #define POINTS 500
 
 int main() {
-	// Create training set for teaching the network how to OR numbers.
+	// Create training set for teaching the network how to operate on numbers.
 	for (int i = 0; i < POINTS; ++i) {
-		int n1 = (int)(2.0 * rand() / double(RAND_MAX));
-		int n2 = (int)(2.0 * rand() / double(RAND_MAX));
-		int t = n1 ^ n2; // should be 0 or 1
-		std::cout << "input: " << n1 << ".0 " << n2 << ".0 " << std::endl;
-		std::cout << "output: " << t << ".0" << std::endl;
+		int a = (int)(2.0 * rand() / double(RAND_MAX));
+		int b = (int)(2.0 * rand() / double(RAND_MAX));
+		int o = a ^ b;
+		std::cout << "input: " << a << " " << b << std::endl;
+		std::cout << "output: " << o << " " << std::endl;
 	}
 }
