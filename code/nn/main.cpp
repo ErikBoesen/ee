06c392c8f6a0914a31adaac97ed1f315d4ca7d38
ErikBoesen <me@erikboesen.com>
2018-03-19 16:43:44 -0400

    Use three output neurons during training

--- code/nn/main.cpp
+++ code/nn/main.cpp
@@ -20,8 +20,9 @@ int main() {
 	vector<int> topology;
 	topology.push_back(2);
 	topology.push_back(4);
+	topology.push_back(5);
+	topology.push_back(4);
 	topology.push_back(3);
-	topology.push_back(1);
 
 	Net network(topology);
 
