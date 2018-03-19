a8d2cf9c3b748d59d36f6eacb401d6e7262e95c4
ErikBoesen <me@erikboesen.com>
2018-03-19 16:42:07 -0400

    Generate AND, OR, and XOR outputs for binary pairs in training

--- code/nn/data_generator.cpp
+++ code/nn/data_generator.cpp
@@ -2,15 +2,17 @@
 #include <cmath>
 #include <cstdlib>
 
-#define POINTS 500
+#define POINTS 1000
 
 int main() {
 	// Create training set for teaching the network how to operate on numbers.
 	for (int i = 0; i < POINTS; ++i) {
 		int a = (int)(2.0 * rand() / double(RAND_MAX));
 		int b = (int)(2.0 * rand() / double(RAND_MAX));
-		int o = a ^ b;
+		int o_and = a & b,
+			o_or  = a | b,
+			o_xor = a ^ b;
 		std::cout << "input: " << a << " " << b << std::endl;
-		std::cout << "output: " << o << " " << std::endl;
+		std::cout << "output: " << o_and << " " << o_or << " " << o_xor << std::endl;
 	}
 }
