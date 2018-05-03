7bd7a928a023496cb7cf28b56f22b3a42e77d36b
ErikBoesen <me@erikboesen.com>
2018-05-03 18:16:12 -0400

    Use mwparserfromhell

--- data/process.py
+++ data/process.py
@@ -1,23 +1,8 @@
 import xml.etree.ElementTree as etree
-import re
-
-# Regular Expression borrowed from https://github.com/fitnr/unwiki
-# TODO: Make your own
-STRIP_RE = re.compile(r"""\[\[(File|Category):[\s\S]+\]\]|
-        \[\[[^|^\]]+\||
-        \[\[|
-        \]\]|
-        \'{2,5}|
-        (<s>|<!--)[\s\S]+(</s>|-->)|
-        {{[\s\S\n]+?}}|
-        <ref>[\s\S]+</ref>|
-        ^(\* *)|
-        ={1,6}""", re.VERBOSE)
+import mwparserfromhell
 
 def strip(text: str) -> str:
-    res = STRIP_RE.sub('', text)
-    res = re.sub('[\n\t ]+', ' ', res)
-    return res
+    return mwparserfromhell.parse(text).strip_code()
 
 print('Loading')
 tree = etree.parse('en.xml')
@@ -27,11 +12,7 @@ root = tree.getroot()
 with open('en.txt', 'w+') as of:
     for ind, child in enumerate(root):
         if child.tag == '{http://www.mediawiki.org/xml/export-0.10/}page' and child[3].tag == '{http://www.mediawiki.org/xml/export-0.10/}revision':
-            try:
-                print(child[0].text)  # Title
-                of.write(strip(child[3][7].text))
-
-            except:
-                pass
+            print('Title:', child[0].text)
+            of.write(strip(child[3][7].text))
         if ind > 100:
             break
