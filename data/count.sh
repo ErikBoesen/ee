f9b7cf75626a35d096a5e552eab022113dc09ae3
ErikBoesen <me@erikboesen.com>
2018-05-29 17:46:45 -0400

    Count files and lines in input data

--- data/count.sh
+++ data/count.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-total=0
+file_total=0
+line_total=0
 for language in $(ls languages); do
-    lcount=$(wc -l languages/$language/* | tail -1 | awk '{print $1}')
-    echo "$language: $lcount"
-    total=$((total+lcount))
+    file_count=$(ls languages/$language | wc -l | awk '{print $1}')
+    line_count=$(wc -l languages/$language/* | tail -1 | awk '{print $1}')
+    echo "$language: $file_count files $line_count lines"
+    file_total=$((file_total+file_count))
+    line_total=$((line_total+line_count))
 done
-echo "Total: $total"
+echo "Total: $file_total files $line_total lines"
