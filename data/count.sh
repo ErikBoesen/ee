351b23169792bab52434823c5bcfd064c16515d4
ErikBoesen <me@erikboesen.com>
2018-05-29 17:51:16 -0400

    More qi in count.sh

--- data/count.sh
+++ data/count.sh
@@ -4,8 +4,8 @@ line_total=0
 for language in $(ls languages); do
     file_count=$(ls languages/$language | wc -l | awk '{print $1}')
     line_count=$(wc -l languages/$language/* | tail -1 | awk '{print $1}')
-    echo "$language: $file_count files $line_count lines"
     file_total=$((file_total+file_count))
     line_total=$((line_total+line_count))
+    echo "$language: $file_count files $line_count lines"
 done
 echo "Total: $file_total files $line_total lines"
