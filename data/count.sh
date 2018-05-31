75b5ff577bfc2731d927cfc5b3a82da2551d51d0
ErikBoesen <me@erikboesen.com>
2018-05-30 22:23:03 -0400

    Count n-grams in count.sh

--- data/count.sh
+++ data/count.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+N=8
 file_total=0
 line_total=0
 char_total=0
@@ -9,6 +10,6 @@ for language in $(ls languages); do
     ((file_total+=file_count))
     ((line_total+=line_count))
     ((char_total+=char_count))
-    echo "$language: $file_count files $line_count lines $char_count characters"
+    echo "$language: $file_count files $line_count lines $char_count characters $((char_count/N)) $N-grams"
 done
-echo "Total: $file_total files $line_total lines $char_total characters"
+echo "Total: $file_total files $line_total lines $char_total characters $((char_total/N)) $N-grams"
