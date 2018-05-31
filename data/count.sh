f3c16257695bdb5656417b6d0784eec5fd206cbb
ErikBoesen <me@erikboesen.com>
2018-05-30 22:21:31 -0400

    Count characters in count.sh too

--- data/count.sh
+++ data/count.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 file_total=0
 line_total=0
+char_total=0
 for language in $(ls languages); do
     file_count=$(ls languages/$language | wc -l | awk '{print $1}')
     line_count=$(wc -l languages/$language/* | tail -1 | awk '{print $1}')
+    char_count=$(wc -c languages/$language/* | tail -1 | awk '{print $1}')
     ((file_total+=file_count))
     ((line_total+=line_count))
-    echo "$language: $file_count files $line_count lines"
+    ((char_total+=char_count))
+    echo "$language: $file_count files $line_count lines $char_count characters"
 done
-echo "Total: $file_total files $line_total lines"
+echo "Total: $file_total files $line_total lines $char_total characters"
