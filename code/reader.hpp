c4e42ff36665a97267bc44de8bc02768e6b9794b
ErikBoesen <me@erikboesen.com>
2018-06-06 14:25:27 -0400

    Remove unused isEof method of Reader

--- code/reader.hpp
+++ code/reader.hpp
@@ -7,7 +7,6 @@ using namespace std;
 class Reader {
 public:
 	Reader(const string language, int language_index, int num_languages);
-	bool isEof() { return this->data_file.eof(); }
 
 	// Returns the number of input values read from the file:
 	int get_ngram(vector<double> &ngram);
