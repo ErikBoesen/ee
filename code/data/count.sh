c2ebefcf1311211aea3fe8ec789381fa56a57a52
ErikBoesen <me@erikboesen.com>
2018-06-06 13:43:15 -0400

    Make count.sh properly use single-file training data layout

--- code/data/count.sh
+++ code/data/count.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 N=8
-file_total=0
 line_total=0
 char_total=0
 for language in $(ls languages); do
-    file_count=$(ls languages/$language | wc -l | awk '{print $1}')
-    line_count=$(wc -l languages/$language/* | tail -1 | awk '{print $1}')
-    char_count=$(wc -c languages/$language/* | tail -1 | awk '{print $1}')
-    ((file_total+=file_count))
+    line_count=$(wc -l languages/$language | awk '{print $1}')
+    char_count=$(wc -c languages/$language | awk '{print $1}')
     ((line_total+=line_count))
     ((char_total+=char_count))
-    echo "$language: $file_count files, $line_count lines, $char_count characters, $((char_count/N)) $N-grams"
+    echo "$language: $line_count lines, $char_count characters, $((char_count/N)) $N-grams"
 done
-echo "Total: $file_total files, $line_total lines, $char_total characters, $((char_total/N)) $N-grams"
+echo "Total: $line_total lines, $char_total characters, $((char_total/N)) $N-grams"
