d2d7995aece27003b761d1e58d5b404577517d01
ErikBoesen <me@erikboesen.com>
2018-09-09 21:52:31 -0400

    code/network.py: get training to work (!)

--- code/network.py
+++ code/network.py
@@ -1,65 +1,41 @@
 from keras.models import Sequential
 from keras.layers import Dense
-import numpy
+import numpy as np
+# TODO: numpy can be used to load CSVs, I believe.
+import pandas as pd
+
 # fix random seed for reproducibility
-numpy.random.seed(7)
+np.random.seed(7)
 
 TRACK_START = 32
 TRACK_END = 126
 
 NUM_LANGS = 10
-TRAINING_EPOCHS = 1000
+TRAINING_EPOCHS = 25  # Again, this is the largest we can do with SQL.
 
 class Reader:
     pass
 
 languages = ["Java", "C", "C++", "Python", "C-sharp", "Visual-Basic-.NET", "PHP", "JavaScript", "SQL", "Ruby"]
 
-readers = [Reader(language) for language in languages]
+input_dim = TRACK_END - TRACK_START;
 
-input_dim = TRACK_END - TRACK_START + 1;
+dataset = np.loadtxt('data.csv', delimiter=',')
+
+X = dataset[:,0:input_dim]
+Y = dataset[:,input_dim:input_dim+NUM_LANGS]
+print(X)
+print(Y)
+
 
 model = Sequential()
 model.add(Dense(48, input_dim=input_dim, activation='relu'))
 model.add(Dense(24, activation='relu'))
 model.add(Dense(NUM_LANGS, activation='sigmoid'))
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-for epoch in range(TRAINING_EPOCHS):
-    for lang_ind in range(NUM_LANGS):
-        for (int lang_ind = 0; lang_ind < NUM_LANGS; ++lang_ind) {
-                // Get new input data and feed it forward
-                readers[lang_ind]->get_rates(rates);
+model.compile(loss='categorical_crossentropy', optimizer='nadam', metrics=['accuracy'])
 
-                network.feed_forward(rates);
+model.fit(X, Y, epochs=TRAINING_EPOCHS, batch_size=NUM_LANGS)
 
-                // Collect the network's results
-                network.get_results(results);
-
-                // Generate correct outputs
-                //reader.get_outputs(targets);
-                targets.clear();
-                for (int field = 0; field < NUM_LANGS; ++field) targets.push_back(0);
-                targets[lang_ind] = 1;
-                assert(targets.size() == topology.back());
-
-                // Backpropogate!
-                network.backpropogate(targets);
-
-                cout << "\033[36mE" << epoch << " (" << languages[lang_ind] << ',' << lang_ind << ") / \033[33m";
-                //print_ngram(rates);
-                cout << "\033[0m > \033[32m";
-                cout << setprecision(PRECISION);
-                print_vector(results);
-                cout << " \033[31merr";
-                print_error(results, targets);
-                cout << endl;
-        }
-}
-
-
-
-
-
-
-
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
