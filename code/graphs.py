a064e145f37cbde05908f56772b4914c164099fc
ErikBoesen <me@erikboesen.com>
2018-09-11 22:23:27 -0400

    code/graphs.py: change figure size

--- code/graphs.py
+++ code/graphs.py
@@ -1,7 +1,9 @@
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 
+figure(figsize=(8,3))
 plt.subplot(1, 2, 1)
 plt.subplot(1, 2, 2)
 plt.savefig('history.png')
