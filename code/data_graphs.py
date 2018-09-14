779ed11134725815dc645321120456c83dc3e064
ErikBoesen <me@erikboesen.com>
2018-09-14 15:51:47 -0400

    Graph character frequency

--- code/data_graphs.py
+++ code/data_graphs.py
@@ -6,9 +6,10 @@ TRACK_START = 32
 TRACK_END = 126
 
 NUM_LANGS = 10
-languages = ["Java", "C", "C++", "Python", "C-sharp", "Visual-Basic-.NET", "PHP", "JavaScript", "Ruby", "R"]
+languages = ['Java', 'C', 'C++', 'Python', 'C-sharp', 'Visual-Basic-.NET', 'PHP', 'JavaScript', 'Ruby', 'R']
 
 text = [open('data/languages/' + language).read() for language in languages]
+x = list(range(TRACK_END - TRACK_START))
 for lang_ind in range(NUM_LANGS):
     rates = [0.0] * (TRACK_END - TRACK_START)
     # Go through each character in file, adding to the rates appropriately
@@ -18,4 +19,11 @@ for lang_ind in range(NUM_LANGS):
     # Turn rates into rates rather than counts
     rates = [rate / len(text[lang_ind]) for rate in rates]
     print(rates)
+    plt.subplot(5, 2, lang_ind + 1)
+    plt.grid(True)
+    plt.title(languages[lang_ind])
+    plt.plot(x, rates, color='#4ead78')
+    plt.xlabel('characters')
+    plt.ylabel('frequency')
 
+plt.savefig('frequencies.png', bbox_inches='tight')
