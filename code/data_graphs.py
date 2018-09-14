48f1cdf31bdcdc4d3087187016696557421f85fa
ErikBoesen <me@erikboesen.com>
2018-09-14 14:56:09 -0400

    Remove data_graphs.py fluff

--- code/data_graphs.py
+++ code/data_graphs.py
@@ -1,4 +1,6 @@
-import csv
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 TRACK_START = 32
 TRACK_END = 126
@@ -9,32 +11,13 @@ N = 1000
 languages = ["Java", "C", "C++", "Python", "C-sharp", "Visual-Basic-.NET", "PHP", "JavaScript", "Ruby", "R"]
 
 text = [open('data/languages/' + language).read() for language in languages]
-data = []
-for epoch in range(EPOCHS):
-    for lang_ind in range(NUM_LANGS):
-        rates = [0.0] * (TRACK_END - TRACK_START)
-        block = text[lang_ind][:N]
-        text[lang_ind] = text[lang_ind][N:]
-        # Go through each character in block, adding to the rates appropriately
-        for c in block:
-            if TRACK_START <= ord(c) < TRACK_END:
-                rates[ord(c) - TRACK_START] += 1
-        print(block)
-        if not block:
-            import sys
-            print('Not enough data for ' + languages[lang_ind])
-            sys.exit(1)
-        # Turn rates into rates rather than counts
-        rates = [rate / N for rate in rates]
-        print(rates)
-        lang_binaries = [0] * NUM_LANGS
-        lang_binaries[lang_ind] = 1
-        data.append(rates + lang_binaries)
+for lang_ind in range(NUM_LANGS):
+    rates = [0.0] * (TRACK_END - TRACK_START)
+    # Go through each character in file, adding to the rates appropriately
+    for c in block:
+        if TRACK_START <= ord(c) < TRACK_END:
+            rates[ord(c) - TRACK_START] += 1
+    # Turn rates into rates rather than counts
+    rates = [rate / N for rate in rates]
+    print(rates)
 
-
-with open('data.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    # Create header row
-    #writer.writerow([chr(c) for c in range(TRACK_START, TRACK_END)] + languages)
-    for row in data:
-        writer.writerow(row)
