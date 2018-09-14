ccae0d5da99daebabfcdd3b6b65bd3ead7e143d7
ErikBoesen <me@erikboesen.com>
2018-09-14 15:42:25 -0400

    Replace all remaining double quotes with single

--- code/data_reader.py
+++ code/data_reader.py
@@ -6,7 +6,7 @@ TRACK_END = 126
 NUM_LANGS = 10
 EPOCHS = 100  # More epochs would be ideal. Visual Basic .NET does not have many solutions on RosettaCode.
 N = 1000
-languages = ["Java", "C", "C++", "Python", "C-sharp", "Visual-Basic-.NET", "PHP", "JavaScript", "Ruby", "R"]
+languages = ['Java', 'C', 'C++', 'Python', 'C-sharp', 'Visual-Basic-.NET', 'PHP', 'JavaScript', 'Ruby', 'R']
 
 text = [open('data/languages/' + language).read() for language in languages]
 data = []
