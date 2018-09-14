ffacd4082e01116df93a57dff8b798f6bf45b372
ErikBoesen <me@erikboesen.com>
2018-09-14 14:59:11 -0400

    code/data_graphs.py: fix language counting

--- code/data_graphs.py
+++ code/data_graphs.py
@@ -6,18 +6,16 @@ TRACK_START = 32
 TRACK_END = 126
 
 NUM_LANGS = 10
-EPOCHS = 100  # More epochs would be ideal. Visual Basic .NET does not have many solutions on RosettaCode.
-N = 1000
 languages = ["Java", "C", "C++", "Python", "C-sharp", "Visual-Basic-.NET", "PHP", "JavaScript", "Ruby", "R"]
 
 text = [open('data/languages/' + language).read() for language in languages]
 for lang_ind in range(NUM_LANGS):
     rates = [0.0] * (TRACK_END - TRACK_START)
     # Go through each character in file, adding to the rates appropriately
-    for c in block:
+    for c in text[lang_ind]:
         if TRACK_START <= ord(c) < TRACK_END:
             rates[ord(c) - TRACK_START] += 1
     # Turn rates into rates rather than counts
-    rates = [rate / N for rate in rates]
+    rates = [rate / len(text[lang_ind]) for rate in rates]
     print(rates)
 
