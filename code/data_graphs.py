6147e18fb48fa2592849df4e453cd98ed7bc7fc4
ErikBoesen <me@erikboesen.com>
2018-09-14 15:54:42 -0400

    Change graph colors across languages

--- code/data_graphs.py
+++ code/data_graphs.py
@@ -7,6 +7,7 @@ TRACK_END = 126
 
 NUM_LANGS = 10
 languages = ['Java', 'C', 'C++', 'Python', 'C-sharp', 'Visual-Basic-.NET', 'PHP', 'JavaScript', 'Ruby', 'R']
+colors = ['#050f42', '#084c61', '#0d94a3', '#d34410', '#ff6e00', '#853037', '#ff3d4e', '#d25f6c', '#143d1d', '#387a4a']
 
 text = [open('data/languages/' + language).read() for language in languages]
 x = list(range(TRACK_END - TRACK_START))
@@ -22,7 +23,7 @@ for lang_ind in range(NUM_LANGS):
     plt.subplot(5, 2, lang_ind + 1)
     plt.grid(True)
     plt.title(languages[lang_ind])
-    plt.plot(x, rates, color='#4ead78')
+    plt.plot(x, rates, color=colors[lang_ind])
     plt.xlabel('characters')
     plt.ylabel('frequency')
 
