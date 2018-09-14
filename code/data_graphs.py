3fcd1f141ee62aa5cbf423acb01d1db14e9da225
ErikBoesen <me@erikboesen.com>
2018-09-14 16:52:55 -0400

    Correct x axis in frequency graphs

--- code/data_graphs.py
+++ code/data_graphs.py
@@ -11,22 +11,23 @@ languages = ['Java', 'C', 'C++', 'Python', 'C-sharp', 'Visual-Basic-.NET', 'PHP'
 colors = ['#050f42', '#084c61', '#0d94a3', '#d34410', '#ff6e00', '#853037', '#ff3d4e', '#d25f6c', '#143d1d', '#387a4a']
 
 text = [open('data/languages/' + language).read() for language in languages]
-x = list(range(TRACK_END - TRACK_START))
-figure(figsize=(16, 8))
+x = list(range(TRACK_END))
+figure(figsize=(18, 3))
 for lang_ind in range(NUM_LANGS):
-    rates = [0.0] * (TRACK_END - TRACK_START)
+    rates = [0.0] * (TRACK_END)
     # Go through each character in file, adding to the rates appropriately
     for c in text[lang_ind]:
         if TRACK_START <= ord(c) < TRACK_END:
-            rates[ord(c) - TRACK_START] += 1
+            rates[ord(c)] += 1
     # Turn rates into rates rather than counts
     rates = [rate / len(text[lang_ind]) for rate in rates]
     print(rates)
     plt.subplot(2, 5, lang_ind + 1)
-    plt.subplots_adjust(top=1.3)
+    plt.subplots_adjust(top=3, bottom=0.5)
     plt.grid(True)
     plt.title(languages[lang_ind])
-    plt.ylim(0, 0.1)
+    plt.xlim(TRACK_START, TRACK_END)
+    plt.ylim(0, 0.07)
     plt.plot(x, rates, color=colors[lang_ind])
     plt.xlabel('characters')
     plt.ylabel('frequency')
